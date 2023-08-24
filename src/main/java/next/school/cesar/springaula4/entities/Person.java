8de74ec758097e6f76e76ce781848994edac5503
Sabiniano <sabiniano.maia@gmail.com>
2023-08-23 22:11:00 -0300

    aula04

---
+++ src/main/java/next/school/cesar/springaula4/entities/Person.java
@@ -0,0 +1,57 @@
+package next.school.cesar.springaula4.entities;
+
+public class Person {
+    
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return int return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
